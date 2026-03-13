f93fd74323be39c9e942dfee16d4d2581ebfbd22
pinguu22 <janmattes.dreyer@icloud.com>
2026-03-13 21:07:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,8 @@ window.APP_CONFIG = {
   supabaseUrl: "https://xmswaxftoteeanzqkbjv.supabase.co",
   supabaseAnonKey: "sb_publishable_CGiWwaJXWLqk_yQIASHXkQ_cNTPNoI5",
   appId: "project90-main"
+
+  mode: "BETA Test Mode",
+  version: "v3.1-beta",
+  lastUpdate: "13.03.2026"
 };
