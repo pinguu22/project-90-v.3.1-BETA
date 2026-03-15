7a544a51d28483312a409f77c9c132ebde885c0e
pinguu22 <janmattes.dreyer@icloud.com>
2026-03-16 00:41:30 +0100

    config.js aktualisieren
    Release Build

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.APP_CONFIG = {
   supabaseAnonKey: "sb_publishable_CGiWwaJXWLqk_yQIASHXkQ_cNTPNoI5",
   appId: "project90-main",
 
-  mode: "LIVE BETA",
-  version: "v3.2-beta",
+  mode: "LIVE Release Build",
+  version: "v3.2",
   lastUpdate: "15.03.2026"
 };
